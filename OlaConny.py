169dc0feea03666c6e85b4f23c3190f4193c2ec2
Fennec89 <fennecfox89@gmail.com>
2013-03-18 23:12:23 +0100

    Rewrote some code so the bot will answer in the correct channel.

--- OlaConny.py
+++ OlaConny.py
@@ -25,38 +25,45 @@ class Bot:
 
     def mainLoop(self):
         'Actions that the bot can do :)'
-        self.commands = "!botty quit, hi botty, hello botty, fortune, ofreda, botty join <channel>, thetime"
+        self.commands = "!Ola-Conny quit, hi Ola-Conny, hello Ola-Conny, fortune, ofreda, Ola-Conny join <channel>, thetime"
         while True:
             self.data = self.irc.recv ( 4096 )
             if self.data.find ( 'PING' ) != -1:
                 self.irc.send ( 'PONG ' + self.data.split() [ 1 ] + '\r\n' )
                 
-            if self.data.find ( '!botty quit' ) != -1:
-                self.irc.send ( 'PRIVMSG ' + self.channel + ' :Fine, if you don\'t want me\r\n' )
+            if self.data.find ( '!Ola-Conny quit' ) != -1:
+            	self.data = self.data.split(' ')[2]
+                self.irc.send ( 'PRIVMSG ' + self.data + ' :Fine, if you don\'t want me\r\n' )
                 self.irc.send ( 'QUIT\r\n' )
                 break
                 
-            if self.data.find ( 'hi botty' ) != -1:
-                self.irc.send ( 'PRIVMSG ' + self.channel + ' :I already said hi...\r\n' )
+            if self.data.find ( 'hi Ola-Conny' ) != -1:
+            	self.data = self.data.split(' ')[2]
+                self.irc.send ( 'PRIVMSG ' + self.data + ' :I already said hi...\r\n' )
                 
-            if self.data.find ( 'hello botty' ) != -1:
-                self.irc.send ( 'PRIVMSG ' + self.channel + ' :I already said hi...\r\n' )
+            if self.data.find ( 'hello Ola-Conny' ) != -1:
+            	self.data = self.data.split(' ')[2]
+                self.irc.send ( 'PRIVMSG ' + self.data + ' :I already said hi...\r\n' )
                 
             if self.data.find ( 'KICK' ) != -1:
-                self.irc.send ( 'JOIN ' + self.channel +'\r\n' )
+            	self.data = self.data.split(' ')[2]
+                self.irc.send ( 'JOIN ' + self.data +'\r\n' )
                 
             if self.data.find ( 'cheese' ) != -1:
-                self.irc.send ( 'PRIVMSG ' + self.channel + ' :WHERE!!!!!!\r\n' )
+            	self.data = self.data.split(' ')[2]
+                self.irc.send ( 'PRIVMSG ' + self.data + ' :WHERE!!!!!!\r\n' )
                 
-            if self.data.find ( 'slaps botty' ) != -1:
-                self.irc.send ( 'PRIVMSG ' + self.channel + ' :This is the Trout Protection Agency. Please put the Trout Down and walk away with your hands in the air.\r\n' )
+            if self.data.find ( 'slaps Ola-Conny' ) != -1:
+            	self.data = self.data.split(' ')[2]
+                self.irc.send ( 'PRIVMSG ' + self.data + ' :This is the Trout Protection Agency. Please put the Trout Down and walk away with your hands in the air.\r\n' )
                 
             if self.data.find ( 'fortune' ) != -1:
-                self.line = os.popen("fortune -n 100").read()      
-                self.irc.send ( 'PRIVMSG ' + self.channel + ' :' + self.line + '\r\n' )
+                self.line = os.popen("fortune -n 100").read()
+                self.data = self.data.split(' ')[2]      
+                self.irc.send ( 'PRIVMSG ' + self.data + ' :' + self.line + '\r\n' )
 
             if self.data.find ( 'ofreda' ) != -1:
-                self.offer = self.data.split(':')[ 2 ].replace(':','').split(' ')[2]
+                self.offer = self.data.split(':')[ 2 ].replace(':','').split(' ')[1]
                 self.offer = self.offer.split("\r\n")[0]
                 msg = "BÖG "
                 msg2 = "GAY "
@@ -67,22 +74,24 @@ class Bot:
                 for k in reversed(range(1,14)):
                     self.irc.send ( 'PRIVMSG ' + self.offer + ' :' + msg2*k + '\r\n' )
                     
-            if self.data.find ( 'botty join' ) != -1:
+            if self.data.find ( 'Ola-Conny join' ) != -1:
                 self.data = self.data.split(':')[2]
                 self.data = self.data.split(' ')[2]
                 self.irc.send ( 'JOIN ' + self.data + '\n' )
                 
-            if self.data.find ( 'botty leave' ) != -1:
+            if self.data.find ( 'Ola-Conny leave' ) != -1:
                 self.irc.send ('QUIT\r\n')
             
             if self.data.find ( 'thetime' ) != -1:
             	self.line = os.popen ( 'date' ).read()
-            	self.irc.send ( 'PRIVMSG ' + self.channel + ' :' + self.line + '\r\n' )
+            	self.data = self.data.split(' ')[2]
+            	self.irc.send ( 'PRIVMSG ' + self.data + ' :' + self.line + '\r\n' )
             
             if self.data.find ( 'commands' ) != -1:
-            	self.irc.send ( 'PRIVMSG ' + self.channel + ' :' + self.commands + '\r\n' )
+            	self.data = self.data.split(' ')[2]
+            	self.irc.send ( 'PRIVMSG ' + self.data + ' :' + self.commands + '\r\n' )
             print self.data
 
-bot = Bot('127.0.0.1',6667,'#Dev','Botty')
+bot = Bot('127.0.0.1',6667,'#Dev','Ola-Conny')
 bot.mainLoop()
 
